845ac96b4990a2fa26124eb598de1b53f9deaf05
Simon Schmidt <simon.schmidt@htp-tel.de>
2017-10-19 15:46:51 +0200

    Cassandra backend

---
+++ cassandra/article.go
@@ -0,0 +1,177 @@
+/*
+Copyright (c) 2017 Simon Schmidt
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+package cassandra
+
+import "github.com/gocassa/gocassa"
+import "github.com/maxymania/fastnntp-polyglot"
+import "github.com/byte-mug/fastnntp/posting"
+import "github.com/davecgh/go-xdr/xdr2"
+import "bytes"
+//import "fmt"
+
+type StoredArticle struct{
+	ArticleOverview `cql:",squash"`
+	Head,Body []byte
+}
+
+func NewStoredArticleTable(ks gocassa.KeySpace) gocassa.Table {
+	return ks.Table("articles",&StoredArticle{},gocassa.Keys{
+		PartitionKeys: []string{"MsgId"},
+	})
+}
+
+type ArticleOverview struct{
+	Subject, Sender, Date, MsgId, Refs []byte
+	Bytes, Lines int64
+}
+func (a *ArticleOverview) Convert() *newspolyglot.ArticleOverview{
+	return &newspolyglot.ArticleOverview{
+		a.Subject,
+		a.Sender,
+		a.Date,
+		a.MsgId,
+		a.Refs,
+		a.Bytes,
+		a.Lines,
+	}
+}
+
+type ArticleStorage struct{
+	ks  gocassa.KeySpace
+	gnv gocassa.Table
+	sat gocassa.Table
+}
+
+func NewArticleStorage(ks gocassa.KeySpace) *ArticleStorage {
+	a := new(ArticleStorage)
+	a.ks  = ks
+	a.gnv = GnvTable(ks)
+	a.sat = NewStoredArticleTable(ks)
+	return a
+}
+func (a *ArticleStorage) Initialize() {
+	a.gnv.CreateIfNotExist()
+	a.sat.CreateIfNotExist()
+}
+
+func (a *ArticleStorage) ArticleDirectStat(id []byte) bool {
+	sat := []StoredArticle{}
+	ok := a.sat.Where(gocassa.Eq("MsgId",id)).Read(sat).Run()!=nil
+	return ok&&len(sat)!=0
+}
+func (a *ArticleStorage) ArticleDirectGet(id []byte, head, body bool) *newspolyglot.ArticleObject {
+	sat := new(StoredArticle)
+	if a.sat.Where(gocassa.Eq("MsgId",id)).ReadOne(sat).Run()!=nil { return nil }
+	obj := new(newspolyglot.ArticleObject)
+	obj.Head = sat.Head
+	obj.Body = sat.Body
+	return obj
+}
+func (a *ArticleStorage) ArticleDirectOverview(id []byte) *newspolyglot.ArticleOverview {
+	sat := new(StoredArticle)
+	if a.sat.Where(gocassa.Eq("MsgId",id)).ReadOne(sat).Run()!=nil { return nil }
+	return sat.ArticleOverview.Convert()
+}
+
+func (a *ArticleStorage) ArticleGroupStat(group []byte, num int64, id_buf []byte) ([]byte, bool) {
+	gnvo := new(GroupNumValue)
+	if getGNV(a.gnv,group,num).ReadOne(&gnvo).Run()!=nil { return nil,false }
+	aov := new(newspolyglot.ArticleOverview)
+	xdr.Unmarshal(bytes.NewReader(gnvo.Value),aov) // Assume the correct format.
+	return aov.MsgId,true
+}
+func (a *ArticleStorage) ArticleGroupGet(group []byte, num int64, head, body bool, id_buf []byte) ([]byte, *newspolyglot.ArticleObject) {
+	id,ok := a.ArticleGroupStat(group,num,id_buf)
+	if !ok { return nil,nil }
+	obj := a.ArticleDirectGet(id,head,body)
+	if obj==nil { return nil,nil }
+	return id,obj
+}
+func (a *ArticleStorage) ArticleGroupOverview(group []byte, first, last int64, targ func(int64, *newspolyglot.ArticleOverview)) {
+	aov := new(newspolyglot.ArticleOverview)
+	getLoopGNV(a.gnv,group,first,last,func(i int64,b []byte) bool {
+		xdr.Unmarshal(bytes.NewReader(b),aov) // Assume the correct format.
+		targ(i,aov)
+		return true
+	})
+}
+func (a *ArticleStorage) ArticleGroupList(group []byte, first, last int64, targ func(int64)) {
+	//fmt.Printf("ArticleGroupList %q %d %d\n",group,first,last)
+	//if last==9223372036854775807
+	getLoopGNV(a.gnv,group,first,last,func(i int64,b []byte) bool {
+		targ(i)
+		return true
+	})
+	
+}
+func (a *ArticleStorage) ArticleGroupMove(group []byte, i int64, backward bool, id_buf []byte) (ni int64, id []byte, ok bool) {
+	buf := []byte(nil)
+	if backward {
+		ni,buf,ok = getPrevGNV(a.sat,group,i)
+	} else {
+		ni,buf,ok = getNextGNV(a.sat,group,i)
+	}
+	if ok {
+		aov := new(newspolyglot.ArticleOverview)
+		xdr.Unmarshal(bytes.NewReader(buf),aov)
+		id = aov.MsgId
+	}
+	return
+}
+
+func (a *ArticleStorage) ArticlePostingPost(headp *posting.HeadInfo, body []byte, ngs [][]byte, numbs []int64) (rejected bool, failed bool, err error) {
+	art := new(StoredArticle)
+	art.Subject = headp.Subject
+	art.Sender  = headp.From
+	art.Date    = headp.Date
+	art.MsgId   = headp.MessageId
+	art.Refs    = headp.References
+	
+	art.Bytes   = int64(len(headp.RAW)+2+len(body))
+	art.Lines   = posting.CountLines(body)
+	
+	art.Head = headp.RAW
+	art.Body = body
+	
+	buf := new(bytes.Buffer)
+	chunk := []byte(nil)
+	xdr.Marshal(buf,&(art.ArticleOverview))
+	chunk = buf.Bytes()
+	
+	op := a.sat.Set(art)
+	for i,ng := range ngs {
+		op = op.Add(a.gnv.Set(mkGNV(ng,numbs[i],chunk)))
+	}
+	if a.ArticleDirectStat(headp.MessageId) { return true,false,nil } // We already have it.
+	if op.Run()!=nil { return false,true,nil }
+	return false,false,nil
+}
+func (a *ArticleStorage) ArticlePostingCheckPost() (possible bool) {
+	return true
+}
+func (a *ArticleStorage) ArticlePostingCheckPostId(id []byte) (wanted bool, possible bool) {
+	if a.ArticleDirectStat(id) { return false,true }
+	return true,true
+}
+
---
+++ cassandra/group.go
@@ -0,0 +1,135 @@
+/*
+Copyright (c) 2017 Simon Schmidt
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+package cassandra
+
+import "github.com/gocassa/gocassa"
+//import "fmt"
+
+func joinUp(buf []byte,s []byte,i int64) []byte {
+	u := uint64(i)
+	buf = append(buf[:0],s...)
+	buf = append(buf,
+	byte(u>>40),
+	byte(u>>32),
+	byte(u>>24),
+	byte(u>>16),
+	byte(u>>8),
+	byte(u))
+	return buf
+}
+
+type GroupNumValue struct{
+	Partit []byte
+	Clustr int
+	OrigNo int64
+	Value  []byte
+}
+func GnvTable(ksp gocassa.KeySpace) gocassa.Table {
+	return ksp.Table("grpov",&GroupNumValue{},gocassa.Keys{
+		PartitionKeys: []string{"Partit"},
+		ClusteringColumns: []string{"Clustr"},
+	})
+}
+
+func mkGNV(group []byte,num int64,value []byte) GroupNumValue {
+	return GroupNumValue{joinUp(nil,group,num>>16),int(num&0xffff),num,value}
+}
+
+func getGNV(tab gocassa.Table,group []byte,num int64) gocassa.Filter {
+	return tab.Where(gocassa.Eq("Partit",joinUp(nil,group,num>>16)),gocassa.Eq("Clustr",int(num&0xffff)))
+}
+
+var getNextGNV_Options = gocassa.Options{
+	Limit: 1,
+}
+func getNextGNV(tab gocassa.Table,group []byte,num int64) (int64,[]byte,bool) {
+	pn := num>>16
+	part := joinUp(nil,group,pn)
+	gnvs := []GroupNumValue{}
+	tab.Where(gocassa.Eq("Partit",part),gocassa.GT("Clustr",int(num&0xffff))).Read(&gnvs).WithOptions(getNextGNV_Options).Run()
+	for i := 0; i<128 ; i++ {
+		if len(gnvs)>0 {
+			return gnvs[0].OrigNo,gnvs[0].Value,true
+		}
+		pn++
+		part = joinUp(part,group,pn)
+		tab.Where(gocassa.Eq("Partit",part)).Read(&gnvs).WithOptions(getNextGNV_Options).Run()
+	}
+	return 0,nil,false
+}
+
+var getPrevGNV_Options = gocassa.Options{
+	ClusteringOrder:[]gocassa.ClusteringOrderColumn{{gocassa.DESC,"Clustr"}},
+	Limit:1,
+}
+func getPrevGNV(tab gocassa.Table,group []byte,num int64) (int64,[]byte,bool) {
+	pn := num>>16
+	part := joinUp(nil,group,pn)
+	gnvs := []GroupNumValue{}
+	tab.Where(gocassa.Eq("Partit",part),gocassa.LT("Clustr",int(num&0xffff))).Read(&gnvs).WithOptions(getPrevGNV_Options).Run()
+	for i := 0; i<128 ; i++ {
+		if len(gnvs)>0 {
+			return gnvs[0].OrigNo,gnvs[0].Value,true
+		}
+		if pn==0 { break } // if we are in partition 0, then stop.
+		pn--
+		part = joinUp(part,group,pn)
+		tab.Where(gocassa.Eq("Partit",part)).Read(&gnvs).WithOptions(getPrevGNV_Options).Run()
+	}
+	return 0,nil,false
+}
+
+func getLoopGNV(tab gocassa.Table,group []byte,first, last int64,fu func(int64,[]byte) bool) {
+	pn := first>>16
+	lpn := last>>16
+	part := joinUp(nil,group,pn)
+	gnvs := []GroupNumValue{}
+	if pn==lpn {
+		tab.Where(gocassa.Eq("Partit",part),gocassa.GTE("Clustr",int(first&0xffff)),gocassa.LTE("Clustr",int(last&0xffff))).Read(&gnvs).Run()
+	}else{
+		tab.Where(gocassa.Eq("Partit",part),gocassa.GTE("Clustr",int(first&0xffff))).Read(&gnvs).Run()
+	}
+	nf := 0
+	for {
+		if len(gnvs)==0 {
+			nf++
+			if nf>256 { break }
+		} else {
+			nf = 0
+		}
+		for _,gnv := range gnvs {
+			if gnv.OrigNo>last { return }
+			if !fu(gnv.OrigNo,gnv.Value) { return }
+		}
+		pn++
+		if pn>lpn { break } // lpn is the last partition having
+		part = joinUp(part,group,pn)
+		if pn==lpn {
+			tab.Where(gocassa.Eq("Partit",part),gocassa.LTE("Clustr",int(last&0xffff))).Read(&gnvs).Run()
+		} else {
+			tab.Where(gocassa.Eq("Partit",part)).Read(&gnvs).Run()
+		}
+	}
+}
+
